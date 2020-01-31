824089b0a693befad4b41875d77b4a410c94efe3
Ramesh Selvaraj <itramji3508@gmail.com>
2020-01-31 15:25:40 +0530

    Comiited

--- SingleSignOn_With_WsFederation/Default.aspx.cs
+++ SingleSignOn_With_WsFederation/Default.aspx.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace SingleSignOn_With_WsFederation
 {
 	public partial class _Default : Page
 	{
+		#region Protecteds
 		protected void Page_Load(object sender, EventArgs e)
 		{
-
 		}
+		#endregion
 	}
 }
